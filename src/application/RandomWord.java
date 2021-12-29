9e73f31a77e09564d68a1789c0118af6ce7f24fe
earl-stephens <stephensearl@hotmail.com>
2021-12-29 15:37:56 -0700

    Add commented code for char datatype

--- src/application/RandomWord.java
+++ src/application/RandomWord.java
@@ -6,6 +6,7 @@ public class RandomWord {
 /* A soulmate is someone who has locks that fit our keys, and keys to fit our locks. When we feel safe enough to open the locks, our truest selves step out and we can be completely and honestly who we are; we can be loved for who we are and not for who we’re pretending to be. Each unveils the best part of the other. No matter what else goes wrong around us, with that one person we’re safe in our own paradise. Our soulmate is someone who shares our deepest longings, our sense of direction. When we’re two balloons, and together our direction is up, chances are we’ve found the right person. Our soulmate is the one who makes life come to life.
  */
 	
+	
 	private String[] words = {"someone", "enough", "completely", "honestly", "pretending", "matter", "around", "paradise", "balloons", "together", "direction"};
 	private String chosenWord;
 	//This is an instance variable in case it's needed again elsewhere in the class
@@ -19,5 +20,22 @@ public class RandomWord {
 	
 	public String toString() {
 		return chosenWord;
+		
+		
 	}
+	/*char is another primitive datatype.  holds a single unicode character
+	initialize a char with single quotes
+	default for char is \u0000
+	cant use null because primitive char is data, not an object
+	char c1 = '\uu2614';
+	char c2 = 'A';
+	boolean equalityCheck = (c1 == c2);
+	
+	* charAt(index) is used to pick out the character in a string
+	 * at the index value given
+	 * String text = "George";
+	 * char c4 = text.charAt(4);
+	 * outputs 'g'
+	 */
+
 }
